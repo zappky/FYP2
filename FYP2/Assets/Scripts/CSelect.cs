b5f6bc7088c77e8028962f844c2316bf78f41a34
zappky <pang_kee_yang@hotmail.com>
2015-05-11 22:50:45 +0800

    Simple gui display and input trigger for interact

--- FYP2/Assets/Scripts/CSelect.cs
+++ FYP2/Assets/Scripts/CSelect.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class CSelect : MonoBehaviour {
 
 	public RaycastHit hit;
+	public float raylength = 10.0f;
+	public bool ongui = true;
+	public bool onguihelper = true;
+	public string interacthelpertext = "'e' to interact";
 	Ray ray;
 
 	// Use this for initialization
@@ -14,13 +18,32 @@ public class CSelect : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		ray = Camera.main.ScreenPointToRay (new Vector3 (Screen.width *0.5f,Screen.height *0.5f,0.0f)); // update the ray positon
-		if (Physics.Raycast (ray, out hit, 10)) //ray cast testing
-		{
-			Debug.Log(hit.collider.gameObject.name);
-			if(hit.collider.gameObject.name.Equals("TestAlarm") == true)//will be changed in future
-			{
+		
+		if (Physics.Raycast (ray, out hit, raylength)) 
+		{ //ray cast testing
+			onguihelper = true;
 
-				hit.collider.gameObject.GetComponent<CTimer>().OnLookInteract();
+			Debug.Log (hit.collider.gameObject.name);
+			if (Input.GetKeyDown ("e")) 
+			{
+				if (hit.collider.gameObject.tag == "Alarm") 
+				{
+					hit.collider.gameObject.GetComponent<CTimer> ().OnLookInteract ();
+				}
+			}
+		} else 
+		{
+			onguihelper = false;
+		}
+	}
+	void OnGUI()
+	{
+		if (ongui == true) 
+		{
+			if(onguihelper == true)
+			{
+				//Debug.Log (interacthelpertext.Length);
+				GUI.Box(new Rect(Screen.width*0.5f,Screen.height*0.95f,interacthelpertext.Length*6.0f,20.0f), interacthelpertext);
 			}
 		}
 	}
