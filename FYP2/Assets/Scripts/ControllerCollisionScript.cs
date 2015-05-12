0c085fc6d401ce0e430a4e3a6c94115aec7d06e5
Hazim <qweasd777@live.com>
2015-05-12 23:55:58 +0800

    Collision response
    Player can now push objects that has a rigidbody component

---
+++ FYP2/Assets/Scripts/ControllerCollisionScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerCollisionScript : MonoBehaviour {
+
+	//private FPSInputController FPSControllerScript;
+	public float pushPower = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnControllerColliderHit(ControllerColliderHit col)
+	{
+		if(col.rigidbody == null || col.rigidbody.isKinematic)
+			return;
+
+		//if (col.gameObject.name == "Ball") //hardcode
+		//{
+		//}
+
+		// to not push obj below player
+		if (col.moveDirection.y < -0.3)
+			return;
+
+		// calc push dir from move dir (push obj to sides only)
+		Vector3 pushDir = new Vector3(col.moveDirection.x, 0, col.moveDirection.z);
+		
+		// get js
+		//FPSControllerScript = this.GetComponent<FPSInputController>();
+		// apply push to obj * player's force (vel)
+		col.rigidbody.velocity = pushDir * pushPower;
+	}
+}
