9bc61497ed6f16054e422c3389edb821dd83d3e1
zappky <pang_kee_yang@hotmail.com>
2015-05-23 23:42:32 +0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 8361e9545be2fa4229a55584a1b3d3165def13a5, reversing
    changes made to 49e1ce6db22d52b4b3a874251c92a73bd80632cf.

--- FYP2/Assets/Scripts/CSelect.cs
+++ FYP2/Assets/Scripts/CSelect.cs
@@ -10,11 +10,13 @@ public class CSelect : MonoBehaviour {
 	public bool display = true;
 	public bool onguihelper = true;
 	public string interacthelpertext = "'e' to interact";
+	public Inventory playerinventory;
 	Ray ray;
 
 	// Use this for initialization
 	void Start () {
-
+		
+		playerinventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
 	}
 	
 	// Update is called once per frame
@@ -25,12 +27,27 @@ public class CSelect : MonoBehaviour {
 		{ //ray cast testing
 			onguihelper = true;
 
-			//Debug.Log (hit.collider.gameObject.name);
 			if (Input.GetButtonDown ("Interact")) 
 			{
-				if (hit.collider.gameObject.tag == "Alarm") 
+				
+				switch(hit.collider.gameObject.tag)
 				{
-					hit.collider.gameObject.GetComponent<CTimer> ().OnLookInteract ();
+					case "Alarm":
+					{
+						hit.collider.gameObject.GetComponent<CTimer> ().OnLookInteract ();
+					}
+					break;
+				}
+			}
+			if (Input.GetButtonDown ("Pick Up")) 
+			{
+				switch(hit.collider.gameObject.tag)
+				{
+				case "Alarm":
+				{
+					playerinventory.AddItem(1);
+				}
+					break;
 				}
 			}
 		} else 
