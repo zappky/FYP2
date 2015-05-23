42f0037a71f686e335da1bab05076b0e1d13f686
zappky <pang_kee_yang@hotmail.com>
2015-05-23 23:58:49 +0800

    Restored darren alert script and Source control config
    Darren, please restore back the gameobject stuff =P sorry for bad merge

--- FYP2/Assets/Scripts/ControllerCollisionScript.cs
+++ FYP2/Assets/Scripts/ControllerCollisionScript.cs
@@ -1,8 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class ControllerCollisionScript : MonoBehaviour {
 
+	//private FPSInputController FPSControllerScript;
 	public float pushPower = 2.0f;
 
 	// Use this for initialization
@@ -15,6 +16,14 @@ public class ControllerCollisionScript : MonoBehaviour {
 
 	void OnControllerColliderHit(ControllerColliderHit col)
 	{
+		//check win
+		if (col.transform.tag == "WinObjective")
+		{
+			//print ("hited");
+			Application.LoadLevel("winscreen");
+		}
+
+		//collision to other objects in the world
 		if(col.rigidbody == null || col.rigidbody.isKinematic)
 			return;
 
@@ -28,7 +37,9 @@ public class ControllerCollisionScript : MonoBehaviour {
 
 		// calc push dir from move dir (push obj to sides only)
 		Vector3 pushDir = new Vector3(col.moveDirection.x, 0, col.moveDirection.z);
-
+		
+		// get js
+		//FPSControllerScript = this.GetComponent<FPSInputController>();
 		// apply push to obj * player's force (vel)
 		col.rigidbody.velocity = pushDir * pushPower;
 	}
