1d8d5c5319d38edc7427500de1ecb315cb3c5c3f
zappky <pang_kee_yang@hotmail.com>
2015-07-06 03:06:19 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	FYP2/Assets/Scripts/DialogTree.cs
    	FYP2/Assets/Scripts/LevelManager.cs
    	FYP2/Assets/gamedata/predefined_inventory.xml
    	FYP2/Assets/gamedata/vendordatas.xml
    	FYP2/FYP2.userprefs

---
+++ FYP2/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerEvent : MonoBehaviour {
+
+	public float SFXdelay = 0;
+	public AudioClip eventSFX;
+
+	bool playSFX;
+	Animator animationEvent;
+
+	void Start() 
+	{
+		playSFX = false;
+		animationEvent = transform.parent.GetComponent<Animator>();
+	}
+	
+	void Update () 
+	{
+		if(playSFX)
+		{
+			SFXdelay -= Time.deltaTime;
+			if(SFXdelay <= 0)
+			{
+				if(eventSFX != null)
+				{
+					playSFX = false;
+					AudioSource.PlayClipAtPoint(eventSFX, transform.parent.position);
+				}
+			}
+		}
+	}
+	
+	public void PlayEvent()
+	{
+		if(!animationEvent.enabled)
+		{
+			animationEvent.enabled = true;			// cheat way... dk how play once - research hw ltr
+			playSFX = true;
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		print ("in trigger area");
+		print (other.tag);
+		if(other.tag == "Player")
+		{
+			if(!animationEvent.enabled)
+			{
+				animationEvent.enabled = true;
+				playSFX = true;
+			}
+		}
+	}
+}
