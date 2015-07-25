692f49c2bf08a6f8f5d2d005d7b7580f7b074ef3
Hazim <qweasd777@live.com>
2015-07-25 12:59:37 +0800

    Grapple update
    - still got problem where player teleports when ascending (rare)

--- FYP2/Assets/Scripts/GrappleScript.cs
+++ FYP2/Assets/Scripts/GrappleScript.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GrappleScript : MonoBehaviour {
 
 	public bool isGrappling = false;
-	public float ascSpeed = 0.1f;			// player's ascend/descend spd (scale spd)
+	public float ascSpeed = 100f;			// player's ascend/descend spd 
+	public float toleranceLength = 2f;		// tolerance length for when player is moving on the rope
 	
 	public GameObject grapplePrefab;
-	// throwing force of grapple		(-, upward force, forward force)
-	//public Vector3 throwForce = new Vector3(0, 400f, 100f);		
-	public float throwForce = 100f;
+	public float throwForce = 100f;			// throwing force of grapple	
 
 	GameObject theGrapple;
 	Transform grappleHook; 
@@ -17,15 +17,13 @@ public class GrappleScript : MonoBehaviour {
 	Transform grappleRope;
 	Transform rightHand;
 
-	float maxRopeScale;
-	float ropeLength;
+	bool reeling;
+	int currentRopeIndex;					// stores index of current rope pt player is on
+	Transform targetRopePt;					// targeted pt to move to on rope, when grappling (for asc movement)
+	List<Transform> targetRopePtList = new List<Transform>();	// list of pts on rope for asc movement on rope
 
 	void Start() {
 		rightHand = Camera.main.transform.GetChild(0);
-
-		//maxRopeScale = grapplePrefab.transform.GetChild(1).localScale.y;
-		ropeLength = grapplePrefab.transform.GetChild(3).GetComponent<SkinnedMeshRenderer>().bounds.size.y;
-		print (ropeLength);
 	}
 
 
@@ -41,100 +39,57 @@ public class GrappleScript : MonoBehaviour {
 		if(theGrapple == null)
 			return;
 
-		grappleEnd.position = rightHand.position;
-
-		// Ascend
-		if(Input.GetButton("Fire2"))
-		{				
-			if(grappleRope.localScale.x > 0.1f)
-			{
-				print (grappleRope.localScale.x);
-				TraverseHierarchy(grappleRope, ascSpeed);	// update rope bones scale
-			}
-		}
-		
-		// Descend
-		if(Input.GetButton("Fire3")) 
-		{					
-			if(grappleRope.localScale.x < ropeLength)
-			{
-				print (grappleRope.localScale.x);
-				TraverseHierarchy(grappleRope, -ascSpeed);	// update rope bones scale
-			}
-		}
-
 		// check if player use grapple successfully (hooks sth)
 		GrappleCollisionScript hook = grappleHook.GetComponent<GrappleCollisionScript>();
 		if(hook.GetGrappleHooked())		
 		{
-			if(!this.GetComponent<CharacterController>().isGrounded)
-				this.GetComponent<Rigidbody>().isKinematic = false;			// enable player rb
+			if(!GetComponent<CharacterController>().isGrounded)
+				GetComponent<Rigidbody>().isKinematic = false;			// enable player rb
 			else
-				this.GetComponent<Rigidbody>().isKinematic = true;			// disable player rb
+				GetComponent<Rigidbody>().isKinematic = true;			// disable player rb
+
+			// Ascend
+			if(Input.GetButton("Fire2") && GetComponent<FixedJoint>() != null)
+			{	
+				reeling = true;
+				//GetComponent<FixedJoint>().connectedBody = null;
+				Destroy(GetComponent<FixedJoint>());
+			}
+
+			if(reeling)
+			{
+				GetComponent<Rigidbody>().isKinematic = true;
+				Vector3 targetPos = targetRopePtList[currentRopeIndex].position;
+
+				transform.position = Vector3.MoveTowards(transform.position, targetPos, 
+				                                         ascSpeed * Time.deltaTime);
+				
+				// if player reached targeted pt on rope, move to next
+				if( (targetPos - transform.position).sqrMagnitude < toleranceLength )
+				{
+					if(currentRopeIndex > 0)
+						--currentRopeIndex;
+					else
+					{
+						reeling = false;
+						gameObject.transform.position = grappleHook.position;
+						gameObject.AddComponent<HingeJoint>().connectedBody = grappleHook.GetComponent<Rigidbody>();
+						Destroy(grappleRope.gameObject);
+						Destroy(theGrapple.transform.FindChild("Render").gameObject);
+						Destroy(grappleEnd.gameObject);
+					}
+				}
+			}
 		}
-//			// Ascend
-//			if(Input.GetButton("Fire2"))
-//			{				
-//				if(this.GetComponent<Rigidbody>().isKinematic)
-//					this.GetComponent<Rigidbody>().isKinematic = false;	
-//
-//				if(grappleRope.localScale.x > 0.1f)
-//				{
-//					print (grappleRope.localScale.x);
-//					TraverseHierarchy(grappleRope, ascSpeed);	// update rope bones scale
-//
-//				}
-//			}
-//
-//			// Descend
-//			if(Input.GetButton("Fire3")) 
-//			{					
-//				if(grappleRope.localScale.x < ropeLength)
-//				{
-//					print (grappleRope.localScale.x);
-//					TraverseHierarchy(grappleRope, -ascSpeed);	// update rope bones scale
-//				}
-//			}
-//		}
 	}
 
-	void TraverseHierarchy(Transform root, float _ascSpeed)
+	void TraverseHierarchy(Transform root)
 	{
-		// rescale
-		root.localScale -= Vector3.right*_ascSpeed;
-		
-		root.localScale = new Vector3(Mathf.Round(root.localScale.x*100f)/100f, 
-		                              root.localScale.y, 
-		                              root.localScale.z);
-
-		// Make sure setting of asc/desc wont over scale 
-		root.localScale = new Vector3(Mathf.Clamp(root.localScale.x, 0.1f, ropeLength), 
-		                              root.localScale.y, 
-		                              root.localScale.z);
-
-		if(root.name == "Bone001")
-		{
-//			// get parent's end(btm) by its collider
-//			Bounds parentColBounds = root.parent.GetComponent<Collider>().bounds;
-//			//+ or -?
-//			Vector3 parentEndPos = new Vector3(parentColBounds.center.x-parentColBounds.extents.x*2,
-//			                                   parentColBounds.center.y,
-//			                                   parentColBounds.center.z);
-//			if(root.name == "Bone002")
-//				print ("current pos: " + root.position + "parent end pos: " + parentEndPos);
-//			// shift to endpos
-//			root.localPosition = parentEndPos;
-		}
+		targetRopePtList.Add(root);
 
 		// move to next transform(child) in hierarchy (if any)
 		if(root.childCount > 0)
-			TraverseHierarchy(root.GetChild(0), _ascSpeed);
-		else
-		{
-			print ("b4" + root.position + " " + grappleEnd.position);
-			root.position = grappleEnd.position;
-			print ("after" + root.position + " " + grappleEnd.position);
-		}
+			TraverseHierarchy(root.GetChild(0));
 	}
 
 	public void FireGrapple()
@@ -142,38 +97,40 @@ public class GrappleScript : MonoBehaviour {
 		isGrappling = true;
 
 		// Spawn grapple on player
-		theGrapple = (GameObject)Instantiate(grapplePrefab, 
-		                                     rightHand.position, 
-		                                     rightHand.rotation);
-		
+		theGrapple = (GameObject)Instantiate(grapplePrefab, rightHand.position, rightHand.rotation);
+
+		// make sure grapple's prefab's hook, rope, end is ordered
 		grappleHook = theGrapple.transform.GetChild(0);
 		grappleRope = theGrapple.transform.GetChild(1);
 		grappleEnd = theGrapple.transform.GetChild(2);
 		grappleEnd.transform.position = rightHand.position;
 
-		this.gameObject.AddComponent<FixedJoint>().connectedBody = grappleEnd.GetComponent<Rigidbody>();
+		// add pts of the rope by traversing the rope hierarchy
+		TraverseHierarchy(grappleRope);
+
+		if(targetRopePtList.Count > 0)
+		{
+			reeling = false;
+			currentRopeIndex = targetRopePtList.Count-1;
+		}
+		
+		gameObject.AddComponent<FixedJoint>().connectedBody = grappleEnd.GetComponent<Rigidbody>();
 
 		// Throw grapple (adding force to hook i.e. front of grapple)
 		Rigidbody theGrappleHook_rb = grappleHook.GetComponent<Rigidbody>();
 		theGrappleHook_rb.AddForce(Camera.main.transform.forward*throwForce-Camera.main.transform.up*throwForce*0.5f,
 		                           ForceMode.Impulse);
-
-		// #TO DO
-		// use raycast to get dist how far player is to grappable obj. 
-		// if dist is > total length of grapple, player cant use grapple (since his grapple wont reach)
-
-		// if raycast dist is within size,
-		// use the dist to spawn grapple alr ascended/descended
-		// (e.g. rayc. dist roughly is 19units length, whereas grapple size is 30u. 
-		// So spawn grapple ascended - i.e. 'shortened' with 19u length)
 	}
 	
 	public void ReleaseGrapple()
 	{
-		isGrappling = false;
-		Destroy(this.GetComponent<FixedJoint>());
+		targetRopePt = null;
+		targetRopePtList.Clear();
+		Destroy(GetComponent<FixedJoint>());
 		Destroy(theGrapple);
+
 		theGrapple = null;
+		isGrappling = false;
 		this.gameObject.GetComponent<Rigidbody>().isKinematic = true;	
 	}
 }
